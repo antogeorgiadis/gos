9f5ec4dd9f1bee63042ee929b34a45730dade47a
Tony Georgiadis <antogeorgiadis@gmail.com>
2019-10-24 18:43:21 +0200

    Html for users list in dashboard

--- src/middleware/renderDashboard.js
+++ src/middleware/renderDashboard.js
@@ -1,4 +1,9 @@
 const renderDashboard = (req, res, next) => {
+  const users = req.members || []
+  const userListHtml = users.map(user => `<li>Name: ${user.name} | Email: ${user.email}</li>`)
+  const html = `<ul>${userListHtml}</ul>`
+
+  res.render(html)
 }
 
 module.exports = renderDashboard
